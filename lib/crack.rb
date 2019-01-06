196ba0e65fcb152e19cda4e65de724bfbda94288
April Dagonese <adagonese@gmail.com>
2019-01-05 17:59:34 -0700

    Adds partially built Crack functionality

--- lib/crack.rb
+++ lib/crack.rb
@@ -5,7 +5,7 @@ module Crack
   end
 
   def calculate_key
-    @split_chars = @ciphertext.split("")
+    split_chars = @ciphertext.split("")
     split_chars.each do |char|
       @alphabet.find_index(char)
     end
@@ -16,10 +16,16 @@ module Crack
     rotation_count = @ciphertext.length % 4
     date_shifts = @dateid_object.date_offsets.values
     rotation_count.times do
-      date_shifts.rotate
+      date_shifts = date_shifts.rotate
     end
-    rotated_date_shifts
-    binding.pry
+    date_shifts
+  end
+
+  def calculate_key_chars
+    control = " end".split("")
+    last_four_chars = @ciphertext.split("")[-4..-1]
+    (0..3).map do |i|
+    end 
   end
 
 end
