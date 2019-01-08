6c5d35f2a51aeb9d673d75fd0cc284afce586dea
April Dagonese <adagonese@gmail.com>
2019-01-08 13:43:44 -0700

    All tests passing, 100% test coverage

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -153,123 +153,123 @@ class EnigmaTest < Minitest::Test
     assert_equal "080119", encoded2[:date]
   end
 
-  # def test_decryption_key_and_date_given
-  #   expected =  { :decryption => "whatever",
-  #                 :key => "88888",
-  #                 :date => "101183" }
-  #   assert_equal expected, @enigma.decrypt("lspiuftg", "88888", "101183")
-  # end
-  #
-  # def test_decryption_key_and_date_given_special_chars
-  #   expected =  { :decryption => "tests!",
-  #                 :key => "88888",
-  #                 :date => "101183" }
-  #   assert_equal expected, @enigma.decrypt("ipgih!", "88888", "101183")
-  # end
-  #
-  # def test_decryption_no_date_given
-  #   expected =  { :decryption => "whatever",
-  #                 :key => "88888",
-  #                 :date => "080119" }
-  #   assert_equal expected, @enigma.decrypt("gpnapcrz", "88888")
-  # end
-  #
-  # def test_it_finds_decode_chars
-  #   @enigma.set_up_enigma("080119")
-  #   @enigma.set_up_shift("11111")
-  #   shift = [15, 12, 17, 12]
-  #
-  #   assert_equal "a", @enigma.find_decode_char("m", 5, shift)
-  #   assert_equal "!", @enigma.find_decode_char("!", 10, shift)
-  # end
-  #
-  # def test_it_decodes_cyphertext
-  #   decryption1 = @enigma.decrypt("krgt", "01234", "101183")
-  #   decryption2 = @enigma.decrypt("lspiuftg", "88888", "101183")
-  #   decryption3 = @enigma.decrypt("@cgx0wt!", "88888", "101183")
-  #
-  #   assert_equal "abcd", decryption1[:decryption]
-  #   assert_equal "whatever", decryption2[:decryption]
-  #   assert_equal "@ssh0le!", decryption3[:decryption]
-  # end
-  #
-  # def test_it_decrypts_package
-  #   @enigma.set_up_enigma("101183")
-  #   @enigma.set_up_shift("11111")
-  #
-  #   expected =  { :decryption => "happiness",
-  #                 :key => "11111",
-  #                 :date => "101183" }
-  #
-  #   assert_equal expected, @enigma.decrypt_package("aphibbxll", "11111", "101183")
-  # end
-  #
-  # def test_it_cracks_keys
-  #   @enigma.set_up_enigma("080119")
-  #   assert_equal "49841", @enigma.crack_key("zwws", "080119")
-  #
-  #   @enigma.set_up_enigma("101183")
-  #   assert_equal "12345", @enigma.crack_key("uebd", "101183")
-  #
-  #   @enigma2.set_up_enigma("111118")
-  #   assert_equal "12345", @enigma2.crack_key("ujwz", "111118")
-  # end
-  #
-  # def test_it_cracks_package
-  #   @enigma.set_up_enigma("101183")
-  #   expected =  { :decryption => "whatever end",
-  #                 :key => "88888",
-  #                 :date => "101183" }
-  #   assert_equal expected, @enigma.crack_package("lspiuftgppbt", "101183")
-  #
-  #   @enigma3.set_up_enigma("111118")
-  #   expected3 =  { :decryption => "abcdefghijkl end",
-  #                 :key => "12345",
-  #                 :date => "111118" }
-  #   assert_equal expected3, @enigma3.crack_package("vglzzkpccotgujwz", "111118")
-  #
-  #   @enigma4.set_up_enigma("111118")
-  #   expected4 =  { :decryption => "hiapril! end",
-  #                 :key => "12345",
-  #                 :date => "111118" }
-  #   assert_equal expected4, @enigma4.crack_package("bnjklnu!ujwz", "111118")
-  # end
-  #
-  # def test_it_cracks_package_special_chars
-  #   @enigma2.set_up_enigma("111118")
-  #   expected2 =  { :decryption => "whatever! end",
-  #                  :key => "12345",
-  #                  :date => "111118" }
-  #   assert_equal expected2, @enigma2.crack_package("qmjoz nm!eniy", "111118")
-  # end
-  #
-  # def test_it_cracks
-  #   expected =  { :decryption => "inspiration end",
-  #                 :key => "98765",
-  #                 :date => "260454" }
-  #   assert_equal expected, @enigma.crack("euofeyxjevjqau ", "260454")
-  #
-  #   expected2 =  { :decryption => "compassion end",
-  #                 :key => "11111",
-  #                 :date => "051257" }
-  #   assert_equal expected2, @enigma2.crack("nzailcgbzyoyyo", "051257")
-  #
-  #   expected3 =  { :decryption => "kindness end",
-  #                 :key => "55473",
-  #                 :date => "101183" }
-  #   assert_equal expected3, @enigma3.crack("umoexittjioe", "101183")
-  # end
-  #
-  # def test_it_cracks_special_chars
-  #   expected =  { :decryption => "laughter! end",
-  #                 :key => "01030",
-  #                 :date => "130385" }
-  #   assert_equal expected, @enigma.crack("umzoqejz!ljvm", "130385")
-  #
-  #   expected =  { :decryption => "*magic* end",
-  #                 :key => "28282",
-  #                 :date => "300983" }
-  #   assert_equal expected, @enigma.crack("*pjqpf*jlqm", "300983")
-  # end
+  def test_decryption_key_and_date_given
+    expected =  { :decryption => "whatever",
+                  :key => "88888",
+                  :date => "101183" }
+    assert_equal expected, @enigma.decrypt("lspiuftg", "88888", "101183")
+  end
+
+  def test_decryption_key_and_date_given_special_chars
+    expected =  { :decryption => "tests!",
+                  :key => "88888",
+                  :date => "101183" }
+    assert_equal expected, @enigma.decrypt("ipgih!", "88888", "101183")
+  end
+
+  def test_decryption_no_date_given
+    expected =  { :decryption => "whatever",
+                  :key => "88888",
+                  :date => "080119" }
+    assert_equal expected, @enigma.decrypt("gpnapcrz", "88888")
+  end
+
+  def test_it_finds_decode_chars
+    @enigma.set_up_enigma("080119")
+    @enigma.set_up_shift("11111")
+    shift = [15, 12, 17, 12]
+
+    assert_equal "a", @enigma.find_decode_char("m", 5, shift)
+    assert_equal "!", @enigma.find_decode_char("!", 10, shift)
+  end
+
+  def test_it_decodes_cyphertext
+    decryption1 = @enigma.decrypt("krgt", "01234", "101183")
+    decryption2 = @enigma.decrypt("lspiuftg", "88888", "101183")
+    decryption3 = @enigma.decrypt("@cgx0wt!", "88888", "101183")
+
+    assert_equal "abcd", decryption1[:decryption]
+    assert_equal "whatever", decryption2[:decryption]
+    assert_equal "@ssh0le!", decryption3[:decryption]
+  end
+
+  def test_it_decrypts_package
+    @enigma.set_up_enigma("101183")
+    @enigma.set_up_shift("11111")
+
+    expected =  { :decryption => "happiness",
+                  :key => "11111",
+                  :date => "101183" }
+
+    assert_equal expected, @enigma.decrypt_package("aphibbxll", "11111", "101183")
+  end
+
+  def test_it_cracks_keys
+    @enigma.set_up_enigma("080119")
+    assert_equal "49841", @enigma.crack_key("zwws", "080119")
+
+    @enigma.set_up_enigma("101183")
+    assert_equal "12345", @enigma.crack_key("uebd", "101183")
+
+    @enigma2.set_up_enigma("111118")
+    assert_equal "12345", @enigma2.crack_key("ujwz", "111118")
+  end
+
+  def test_it_cracks_package
+    @enigma.set_up_enigma("101183")
+    expected =  { :decryption => "whatever end",
+                  :key => "88888",
+                  :date => "101183" }
+    assert_equal expected, @enigma.crack_package("lspiuftgppbt", "101183")
+
+    @enigma3.set_up_enigma("111118")
+    expected3 =  { :decryption => "abcdefghijkl end",
+                  :key => "12345",
+                  :date => "111118" }
+    assert_equal expected3, @enigma3.crack_package("vglzzkpccotgujwz", "111118")
+
+    @enigma4.set_up_enigma("111118")
+    expected4 =  { :decryption => "hiapril! end",
+                  :key => "12345",
+                  :date => "111118" }
+    assert_equal expected4, @enigma4.crack_package("bnjklnu!ujwz", "111118")
+  end
+
+  def test_it_cracks_package_special_chars
+    @enigma2.set_up_enigma("111118")
+    expected2 =  { :decryption => "whatever! end",
+                   :key => "12345",
+                   :date => "111118" }
+    assert_equal expected2, @enigma2.crack_package("qmjoz nm!eniy", "111118")
+  end
+
+  def test_it_cracks
+    expected =  { :decryption => "inspiration end",
+                  :key => "98765",
+                  :date => "260454" }
+    assert_equal expected, @enigma.crack("euofeyxjevjqau ", "260454")
+
+    expected2 =  { :decryption => "compassion end",
+                  :key => "11111",
+                  :date => "051257" }
+    assert_equal expected2, @enigma2.crack("nzailcgbzyoyyo", "051257")
+
+    expected3 =  { :decryption => "kindness end",
+                  :key => "55473",
+                  :date => "101183" }
+    assert_equal expected3, @enigma3.crack("umoexittjioe", "101183")
+  end
+
+  def test_it_cracks_special_chars
+    expected =  { :decryption => "laughter! end",
+                  :key => "01030",
+                  :date => "130385" }
+    assert_equal expected, @enigma.crack("umzoqejz!ljvm", "130385")
+
+    expected =  { :decryption => "*magic* end",
+                  :key => "28282",
+                  :date => "300983" }
+    assert_equal expected, @enigma.crack("*pjqpf*jlqm", "300983")
+  end
 
 end
